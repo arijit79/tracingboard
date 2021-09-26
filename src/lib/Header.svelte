e4748362458f53528aa5137efa7bda4a96bbd075
Arijit Dey <arijit79@protonmail.com>
2021-09-26 11:50:09 +0530

    Add initial files

---
+++ src/lib/Header.svelte
@@ -0,0 +1,43 @@
+<header>
+    <span>Board</span>
+    <button>Add Feed</button>
+</header>
+
+<style lang="scss">
+    header {
+        width: 100%;
+        box-shadow: 0 5px 5px rgb(200, 200, 200);
+        height: 3.5rem;
+        line-height: 3.25rem;
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
+
+        --primary-color: rgb(0, 135, 215);
+        --font-color: white;
+
+        span {
+            margin-left: 5rem;
+            font-size: 2.25rem;
+            font-weight: 1000;
+        }
+
+        button {
+            margin-right: 10rem;
+            height: 2.5rem;
+            margin-top: 0.6%;
+            background-color: var(--primary-color);
+            color: var(--font-color);
+            padding: 0 1rem 0 1rem;
+            font-weight: bold;
+            outline: none;
+            border: 2px solid var(--primary-color);
+            transition: background 0.1s;
+
+            &:hover {
+                background-color: var(--font-color);
+                color: var(--primary-color);
+            }
+        }
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Header from "$lib/Header.svelte";
+</script>
+
+<Header />
+<main><span>No Feed to Show</span></main>
+
+<style lang="scss">
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 3.5rem);
+
+        span {
+            // display: block;
+            font-size: 4rem;
+            font-weight: bold;
+            color: rgb(150, 150, 150);
+        }
+    }
+</style>
